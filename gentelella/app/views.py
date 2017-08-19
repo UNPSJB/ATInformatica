3fde3691fe2409dc4b21a24a8d27e590cc6ac86b
Kevin Soto <sotokevin92@users.noreply.github.com>
2017-08-19 20:37:38 -0300

    Submit de AM Cliente como POST
    Experimento con el template de cliente_detail y views.py para entender mejor los requests - el botón Submit envía los datos del cliente por POST.

--- gentelella/app/views.py
+++ gentelella/app/views.py
@@ -12,6 +12,11 @@ def gentella_html(request):
     context = {}
     # The template to be loaded as per gentelella.
     # All resource paths for gentelella end in .html.
+    print(">> REQUEST HTTP: " + str(request.method))
+    if request.method == 'GET':
+        print(request.GET)
+    if request.method == 'POST':
+        print(request.POST)
 
     # Pick out the html file name from the url. And load that template.
     load_template = request.path.split('/')[-1]
