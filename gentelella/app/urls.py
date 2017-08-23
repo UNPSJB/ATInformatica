bbf86546e2640ec2356d27861f4d067cbc8750f6
papablo <pabloatm980@gmail.com>
2017-08-23 11:49:34 -0300

    reruteo dentro del gentelella

--- gentelella/app/urls.py
+++ gentelella/app/urls.py
@@ -5,8 +5,8 @@ urlpatterns = [
     # Matches any html file - to be used for gentella
     # Avoid using your .html in your resources.
     # Or create a separate django app.
-    url(r'^.*\.html', views.gentella_html, name='gentella'),
 
     # The home page
     url(r'^$', views.index, name='index'),
+    url(r'^.*\.html', views.gentella_html, name='gentella'),
 ]
