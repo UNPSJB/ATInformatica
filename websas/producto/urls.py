d4f98bb7b205b922161b7c4672bce7db9d1508eb
LucianoFromTrelew <lucianoserruya@hotmail.com>
2017-09-05 19:22:45 -0300

    subiendo archivos static

--- websas/producto/urls.py
+++ websas/producto/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     # Matches any html file - to be used for gentella
     # Avoid using your .html in your resources.
     # Or create a separate django app.
-    url(r'^productos.html$', views.producto, name='producto'),
+    url(r'^productos$', views.producto, name='producto'),
     url(r'^producto_detail.html', views.producto_detail, name='producto_detail')
 
 ]
