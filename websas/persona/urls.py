37bb84cd5ecf0450545867b2e803265c06b80d4d
LucianoFromTrelew <lucianoserruya@hotmail.com>
2017-09-05 22:20:12 -0300

    urls

---
+++ websas/persona/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    # Matches any html file - to be used for gentella
+    # Avoid using your .html in your resources.
+    # Or create a separate django app.
+
+    # The home page
+    url(r'^$', views.persona, name='persona')
+]
