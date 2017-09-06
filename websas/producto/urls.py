e03470e43253b467c8fea567bb28155089575f55
papablo <pabloatm980@gmail.com>
2017-09-05 23:02:41 -0300

    arreglo de urls, plantilla de ingreso de stock

--- websas/producto/urls.py
+++ websas/producto/urls.py
@@ -5,7 +5,8 @@ urlpatterns = [
     # Matches any html file - to be used for gentella
     # Avoid using your .html in your resources.
     # Or create a separate django app.
-    url(r'^productos$', views.producto, name='producto'),
-    url(r'^producto_detail.html', views.producto_detail, name='producto_detail')
+    url(r'^$', views.producto, name='producto'),
+    url(r'^producto_detail.html', views.producto_detail, name='producto_detail'),
+    url(r'^stock', views.stock, name='stock')
 
 ]
--- websas/producto/views.py
+++ websas/producto/views.py
@@ -10,15 +10,18 @@ def producto(request):
     # Ver el tema de si es modificación o creación
     
     # Pick out the html file name from the url. And load that template.
-    load_template = request.path.split('/')[-1] + '.html'
-    
-    template = loader.get_template('producto/' + load_template)
+    template = loader.get_template('producto/productos.html')
     return HttpResponse(template.render(context, request))
 
 def producto_detail(request):
     context = {}
     # Pick out the html file name from the url. And load that template.
-    load_template = request.path.split('/')[-1] 
     
-    template = loader.get_template('producto/' + load_template)
+    template = loader.get_template('producto/producto_detail.html')
     return HttpResponse(template.render(context, request))
+
+def stock(request):
+    context = {}
+
+    template = loader.get_template('producto/stock.html')
+    return HttpResponse(template.render(context,request))
