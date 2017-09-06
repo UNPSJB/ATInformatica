8b33e005821769a490002738a1515c0d22f822eb
papablo <pabloatm980@gmail.com>
2017-09-05 22:21:54 -0300

    hay aguante si pintan los guantes

---
+++ websas/rubro/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    # Matches any html file - to be used for gentella
+    # Avoid using your .html in your resources.
+    # Or create a separate django app.
+
+]
