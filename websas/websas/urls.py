579fd846095a4755fea65a4e5664a86287706084
LucasKrmpotic <lucas_krmpotic@yahoo.com.ar>
2017-09-07 13:37:12 -0300

    login funcional

--- websas/websas/urls.py
+++ websas/websas/urls.py
@@ -28,7 +28,7 @@ urlpatterns = [
     url(r'^rubro/', include('rubro.urls')),
     url(r'^servicio/', include('servicio.urls')),
     url(r'^usuario/', include('usuario.urls')),
-    url(r'accounts/login/$', views.login, name='login'),
-    url(r'accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
+    url(r'^accounts/login/$', views.login, name='login'),
+    url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
 
 ]
