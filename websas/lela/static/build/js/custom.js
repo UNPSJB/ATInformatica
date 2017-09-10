90aafa1127509647df6e1848f27e9bf0e67b24dd
Kevin Soto <sotokevin92@users.noreply.github.com>
2017-09-10 17:17:24 -0300

    botones tabla tecnicos

--- websas/lela/static/build/js/custom.js
+++ websas/lela/static/build/js/custom.js
@@ -2604,19 +2604,25 @@ function init_DataTables() {
         responsive: true,
         dom: "Bfrtip",
         buttons: [{
+            text: "<b>Nuevo técnico</b>",
+            action: function() {
+                // TODO: si es necesario generar el form de nuevo técnico
+                // como modal, este botón puede dispararlo
+                // Por ahora sólo pega a crear técnico
+                location.href = "/tecnico/crear";
+            },
+            className: "btn-sm btn-info"
+        }, {
             extend: "copy",
+            text: "Copiar tabla",
             className: "btn-sm"
         }, {
             extend: "csv",
-            className: "btn-sm"
-        }, {
-            extend: "excel",
-            className: "btn-sm"
-        }, {
-            extend: "pdfHtml5",
+            text: "Exportar tabla a CSV",
             className: "btn-sm"
         }, {
             extend: "print",
+            text: "Imprimir tabla",
             className: "btn-sm"
         }, ],
     });
