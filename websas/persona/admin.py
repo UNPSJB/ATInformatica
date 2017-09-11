f4c16d9a1102a284fd19f5bb70d14486c6b894a1
papablo <pabloatm980@gmail.com>
2017-09-11 01:45:47 -0300

    admins

--- websas/persona/admin.py
+++ websas/persona/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Persona,Rol,Tecnico
 
 # Register your models here.
+
+admin.site.register(Persona)
+admin.site.register(Tecnico)
--- websas/usuario/admin.py
+++ websas/usuario/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Usuario
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
+admin.site.register(Usuario,UserAdmin)
