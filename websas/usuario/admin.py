260c5e1fcec22310b4f70465a550e5d5d42668fc
papablo <pabloatm980@gmail.com>
2017-09-11 02:15:30 -0300

    admin de usuario permite vincular

--- websas/usuario/admin.py
+++ websas/usuario/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Usuario
+from .models import Usuario, MyUserAdmin
+
 from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
-admin.site.register(Usuario,UserAdmin)
+admin.site.register(Usuario,MyUserAdmin)
