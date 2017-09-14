c6510418203740f6626ce19024434d678fc8822f
LucasKrmpotic <lucas_krmpotic@yahoo.com.ar>
2017-09-14 14:07:20 -0300

    crud de productos completo

--- websas/producto/urls.py
+++ websas/producto/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from .views import ProductoCreate, ProductoList 
+from .views import ProductoCreate, ProductoList, ProductoDelete, ProductoUpdate 
 
 urlpatterns = [
     # Matches any html file - to be used for gentella
@@ -7,4 +7,6 @@ urlpatterns = [
     # Or create a separate django app.
     url(r'^crear$', ProductoCreate.as_view(), name="producto_crear"),
     url(r'^listar$', ProductoList.as_view(), name="producto_listar"),
+    url(r'^editar/(?P<pk>\d+)$', ProductoUpdate.as_view(), name="producto_editar"),
+    url(r'^eliminar/(?P<pk>\d+)$', ProductoDelete.as_view(), name="producto_eliminar"),
 ]
--- websas/producto/views.py
+++ websas/producto/views.py
@@ -3,7 +3,7 @@ from django.template import loader
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView
 from django.core.urlresolvers import reverse_lazy
 from .models import Producto
 from .forms import ProductoForm
@@ -18,3 +18,14 @@ class ProductoCreate(CreateView):
 class ProductoList(ListView):
     model = Producto
     template_name = 'producto/productos.html'
+
+class ProductoUpdate(UpdateView):
+    model = Producto
+    template_name = 'producto/producto_detail.html'
+    form_class = ProductoForm
+    success_url = reverse_lazy('producto:producto_listar')
+
+class ProductoDelete(DeleteView):
+    model = Producto
+    template_name = 'producto/producto_delete.html'
+    success_url = reverse_lazy('producto:producto_listar')
