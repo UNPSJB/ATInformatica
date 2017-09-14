0e7995576db22f664ac40cb34d084c4cc9c3e73f
LucasKrmpotic <lucas_krmpotic@yahoo.com.ar>
2017-09-14 17:37:37 -0300

    reordenando javascripts

--- websas/lela/static/build/js/custom.js
+++ websas/lela/static/build/js/custom.js
@@ -2600,61 +2600,11 @@ function init_DataTables() {
     }();
     */
 
-    $("#datatable-tecnicos").DataTable({
-        responsive: true,
-        dom: "Bfrtip",
-        buttons: [{
-            text: "<b>Nuevo técnico</b>",
-            action: function() {
-                // TODO: si es necesario generar el form de nuevo técnico
-                // como modal, este botón puede dispararlo
-                // Por ahora sólo pega a crear técnico
-                location.href = "/tecnico/crear";
-            },
-            className: "btn-sm btn-info"
-        }, {
-            extend: "copy",
-            text: "Copiar tabla",
-            className: "btn-sm"
-        }, {
-            extend: "csv",
-            text: "Exportar tabla a CSV",
-            className: "btn-sm"
-        }, {
-            extend: "print",
-            text: "Imprimir tabla",
-            className: "btn-sm"
-        }, ],
-    });
+
 
     $('#datatable').dataTable();
 
-    $("#datatable-clientes").DataTable({
-        responsive: true,
-        dom: "Bfrtip",
-        buttons: [{
-            text: "<b>Nuevo cliente</b>",
-            action: function() {
-                // TODO: si es necesario generar el form de nuevo técnico
-                // como modal, este botón puede dispararlo
-                // Por ahora sólo pega a crear técnico
-                location.href = "/cliente/crear";
-            },
-            className: "btn-sm btn-info"
-        }, {
-            extend: "copy",
-            text: "Copiar tabla",
-            className: "btn-sm"
-        }, {
-            extend: "csv",
-            text: "Exportar tabla a CSV",
-            className: "btn-sm"
-        }, {
-            extend: "print",
-            text: "Imprimir tabla",
-            className: "btn-sm"
-        }, ],
-    });
+
 
 
     var tabla = $('#datatable-keytable').DataTable({
---
+++ websas/producto/static/producto/js/producto.js
@@ -0,0 +1,26 @@
+$("#datatable-producto").DataTable({
+    responsive: true,
+    dom: "Bfrtip",
+    buttons: [{
+        text: "<b>Nuevo producto</b>",
+        action: function() {
+            // TODO: si es necesario generar el form de nuevo técnico
+            // como modal, este botón puede dispararlo
+            // Por ahora sólo pega a crear técnico
+            location.href = "/producto/crear";
+        },
+        className: "btn-sm btn-info"
+    }, {
+        extend: "copy",
+        text: "Copiar tabla",
+        className: "btn-sm"
+    }, {
+        extend: "csv",
+        text: "Exportar tabla a CSV",
+        className: "btn-sm"
+    }, {
+        extend: "print",
+        text: "Imprimir tabla",
+        className: "btn-sm"
+    }, ],
+});
