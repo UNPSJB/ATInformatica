7d1ab45040b3841d346eb25cf057bd9c2233cd01
LucasKrmpotic <lucas_krmpotic@yahoo.com.ar>
2017-09-14 14:01:00 -0300

    productos listview y template

--- websas/producto/views.py
+++ websas/producto/views.py
@@ -3,7 +3,7 @@ from django.template import loader
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
-from django.views.generic import CreateView, TemplateView
+from django.views.generic import CreateView, ListView
 from django.core.urlresolvers import reverse_lazy
 from .models import Producto
 from .forms import ProductoForm
@@ -15,5 +15,6 @@ class ProductoCreate(CreateView):
     form_class = ProductoForm
     success_url = reverse_lazy('producto:producto_listar')
 
-class ProductoList(TemplateView):
+class ProductoList(ListView):
+    model = Producto
     template_name = 'producto/productos.html'
