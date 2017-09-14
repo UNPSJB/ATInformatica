d0057cd6e8501ba553e88231e97a5c240ee0daf3
LucasKrmpotic <lucas_krmpotic@yahoo.com.ar>
2017-09-14 12:29:11 -0300

    crud de tipos de servicio terminado

--- websas/servicio/urls.py
+++ websas/servicio/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
-from .views import ServicioCreate, ServicioList 
+from .views import TipoServicioCreate, TipoServicioList, TipoServicioUpdate, TipoServicioDelete
 
 urlpatterns = [
     # Matches any html file - to be used for gentella
     # Avoid using your .html in your resources.
     # Or create a separate django app.
-    url(r'^crear/', ServicioCreate.as_view(), name="servicio_crear"),
-    url(r'^servicio/', ServicioList.as_view(), name="servicio_listar"),
+    url(r'^crear$', TipoServicioCreate.as_view(), name="servicio_crear"),
+    url(r'^listar$', TipoServicioList.as_view(), name="servicio_listar"),
+    url(r'^editar/(?P<pk>\d+)$', TipoServicioUpdate.as_view(), name="servicio_editar"),
+    url(r'^eliminar/(?P<pk>\d+)$', TipoServicioDelete.as_view(), name="servicio_eliminar"),
 ]
--- websas/servicio/views.py
+++ websas/servicio/views.py
@@ -1,14 +1,26 @@
-from django.views.generic import TemplateView, CreateView
+from django.views.generic import TemplateView, CreateView, ListView, UpdateView, DeleteView
 from django.core.urlresolvers import reverse_lazy
 from .models import TipoServicio
 from .forms import TipoServicioForm
 
 # Create your views here.
-class ServicioCreate(CreateView):
+class TipoServicioCreate(CreateView):
     model = TipoServicio
     template_name = 'servicio/servicio_detail.html'
     form_class = TipoServicioForm
     success_url = reverse_lazy('servicio:servicio_listar')
 
-class ServicioList(TemplateView):
+class TipoServicioList(ListView):
+    model = TipoServicio
     template_name = 'servicio/servicios.html'
+
+class TipoServicioUpdate(UpdateView):
+    model = TipoServicio
+    template_name = 'servicio/servicio_detail.html'
+    form_class = TipoServicioForm
+    success_url = reverse_lazy('servicio:servicio_listar')
+
+class TipoServicioDelete(DeleteView):
+    model = TipoServicio
+    template_name = 'servicio/servicio_delete.html'
+    success_url = reverse_lazy('servicio:servicio_listar')
