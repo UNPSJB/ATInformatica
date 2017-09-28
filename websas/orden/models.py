33e842ec5264242e29e8b4fea79629a9f9bf41fd
Pablo <pabloatm980@gmail.com>
2017-09-28 19:55:45 -0300

    class-based views orden

--- websas/orden/models.py
+++ websas/orden/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from persona.models import Cliente, Tecnico
+from rubro.models import Rubro
+from servicio.models import TipoServicio
 
 # Create your models here.
+class Orden(models.Model):
+
+    cliente = models.ForeignKey(
+        Cliente,
+        null=True
+    )
+
+    rubro = models.ForeignKey(
+        Rubro
+    )
+
+    equipo = models.ForeignKey(
+        "Equipo",
+        null=True
+    )
+
+    tipo_servicio = models.ForeignKey(
+        TipoServicio
+    )
+
+    tecnico = models.ForeignKey(
+        Tecnico
+    )
+
+class Equipo(models.Model):
+    nro_serie = models.IntegerField(unique=True)
+    decripcion = models.CharField(max_length=250)
+
