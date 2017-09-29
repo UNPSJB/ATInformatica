bb605795f18b087635222da5d9a64be805b32e27
Kevin Soto <sotokevin92@users.noreply.github.com>
2017-09-28 22:27:20 -0300

    retirado parsley del custom.js
    para que no joda en la consola con formularios que no usamos y estaban en el demo de gentelella

--- websas/lela/static/build/js/custom.js
+++ websas/lela/static/build/js/custom.js
@@ -964,52 +964,53 @@ function init_autosize() {
 
 /* PARSLEY */
 
-function init_parsley() {
+// function init_parsley() {
 
-    if (typeof(parsley) === 'undefined') {
-        return;
-    }
-    console.log('init_parsley');
+//     if (typeof(parsley) === 'undefined') {
+//         return;
+//     }
+//     console.log('init_parsley');
 
-    $ /*.listen*/ ('parsley:field:validate', function() {
-        validateFront();
-    });
-    $('#demo-form .btn').on('click', function() {
-        $('#demo-form').parsley().validate();
-        validateFront();
-    });
-    var validateFront = function() {
-        if (true === $('#demo-form').parsley().isValid()) {
-            $('.bs-callout-info').removeClass('hidden');
-            $('.bs-callout-warning').addClass('hidden');
-        } else {
-            $('.bs-callout-info').addClass('hidden');
-            $('.bs-callout-warning').removeClass('hidden');
-        }
-    };
+//     $ /*.listen*/ ('parsley:field:validate', function() {
+//         validateFront();
+//     });
+//     $('#demo-form .btn').on('click', function() {
+//         $('#demo-form').parsley().validate();
+//         validateFront();
+//     });
+//     var validateFront = function() {
+//         if (true === $('#demo-form').parsley().isValid()) {
+//             $('.bs-callout-info').removeClass('hidden');
+//             $('.bs-callout-warning').addClass('hidden');
+//         } else {
+//             $('.bs-callout-info').addClass('hidden');
+//             $('.bs-callout-warning').removeClass('hidden');
+//         }
+//     };
 
-    $ /*.listen*/ ('parsley:field:validate', function() {
-        validateFront();
-    });
-    $('#demo-form2 .btn').on('click', function() {
-        $('#demo-form2').parsley().validate();
-        validateFront();
-    });
-    var validateFront = function() {
-        if (true === $('#demo-form2').parsley().isValid()) {
-            $('.bs-callout-info').removeClass('hidden');
-            $('.bs-callout-warning').addClass('hidden');
-        } else {
-            $('.bs-callout-info').addClass('hidden');
-            $('.bs-callout-warning').removeClass('hidden');
-        }
-    };
 
-    try {
-        hljs.initHighlightingOnLoad();
-    } catch (err) {}
+//     $ /*.listen*/ ('parsley:field:validate', function() {
+//         validateFront();
+//     });
+//     $('#demo-form2 .btn').on('click', function() {
+//         $('#demo-form2').parsley().validate();
+//         validateFront();
+//     });
+//     var validateFront = function() {
+//         if (true === $('#demo-form2').parsley().isValid()) {
+//             $('.bs-callout-info').removeClass('hidden');
+//             $('.bs-callout-warning').addClass('hidden');
+//         } else {
+//             $('.bs-callout-info').addClass('hidden');
+//             $('.bs-callout-warning').removeClass('hidden');
+//         }
+//     };
 
-};
+//     try {
+//         hljs.initHighlightingOnLoad();
+//     } catch (err) {}
+
+// };
 
 
 /* INPUTS */
@@ -5109,7 +5110,7 @@ $(document).ready(function() {
     init_IonRangeSlider();
     init_ColorPicker();
     init_TagsInput();
-    init_parsley();
+    // init_parsley();
     init_daterangepicker();
     init_daterangepicker_right();
     init_daterangepicker_single_call();
@@ -5122,7 +5123,7 @@ $(document).ready(function() {
     init_skycons();
     init_select2();
     init_validator();
-    init_DataTables();
+    // init_DataTables();
     init_chart_doughnut();
     init_gauge();
     init_PNotify();
