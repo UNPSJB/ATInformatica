740bd542b02369b7d412dd93cf2a00f12b4dc36d
Kevin Soto <sotokevin92@users.noreply.github.com>
2017-09-29 07:26:19 -0300

    vista detalle y formulario - Producto

--- websas/producto/urls.py
+++ websas/producto/urls.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.decorators import login_required
 from django.conf.urls import url
-from .views import ProductoCreate, ProductoList, ProductoDelete, ProductoUpdate 
+from .views import ProductoCreate, ProductoList, ProductoDelete, ProductoUpdate, ProductoDetail
 
 urlpatterns = [
     url(r'^crear$', login_required(ProductoCreate.as_view(), login_url='usuario:login'), name="producto_crear"),
     url(r'^listar$', login_required(ProductoList.as_view(), login_url='usuario:login'), name="producto_listar"),
     url(r'^editar/(?P<pk>\d+)$', login_required(ProductoUpdate.as_view(), login_url='usuario:login'), name="producto_editar"),
     url(r'^eliminar/(?P<pk>\d+)$', login_required(ProductoDelete.as_view(), login_url='usuario:login'), name="producto_eliminar"),
+    url(r'^ver/(?P<pk>\d+)$', login_required(ProductoDetail.as_view(), login_url='usuario:login'), name="producto_ver"),
 ]
--- websas/producto/views.py
+++ websas/producto/views.py
@@ -4,15 +4,15 @@ from django.http import HttpResponse
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import permission_required
 
-from django.views.generic import CreateView, ListView, UpdateView, DeleteView
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView, DetailView
 from django.core.urlresolvers import reverse_lazy
 from .models import Producto
-from .forms import ProductoForm
+from .forms import ProductoForm, ProductoUpdateForm
 # Create your views here.
 
 class ProductoCreate(CreateView):
     model = Producto
-    template_name = 'producto/producto_detail.html'
+    template_name = 'producto/producto_form.html'
     form_class = ProductoForm
     success_url = reverse_lazy('producto:producto_listar')
 
@@ -26,8 +26,8 @@ class ProductoList(ListView):
 
 class ProductoUpdate(UpdateView):
     model = Producto
-    template_name = 'producto/producto_detail.html'
-    form_class = ProductoForm
+    template_name = 'producto/producto_form.html'
+    form_class = ProductoUpdateForm
     success_url = reverse_lazy('producto:producto_listar')
 
     @method_decorator(permission_required('producto.change_producto', login_url='producto:producto_listar'))
@@ -42,3 +42,9 @@ class ProductoDelete(DeleteView):
     @method_decorator(permission_required('producto.delete_producto', login_url='producto:producto_listar'))
     def post(self, request, *args, **kwargs):
         return super().post(request, *args, **kwargs)
+
+class ProductoDetail(DetailView):
+    model = Producto
+    context_object_name = 'producto'
+    template_name = 'producto/producto_detail.html'
+    success_url = reverse_lazy('producto:producto_listar')
