5a9eaa12a6117e14a3eb2f5f6993a0e66485818a
Kevin Soto <sotokevin92@users.noreply.github.com>
2017-09-29 07:27:36 -0300

    form.py que me olvide - Producto

--- websas/producto/forms.py
+++ websas/producto/forms.py
@@ -19,13 +19,59 @@ class ProductoForm(forms.ModelForm):
             'marca':'Marca',
             'stock_minimo':'Stock minimo',
             'stock':'Stock actual',
-            'precio':'Precio',          
+            'precio':'Precio $',          
+        }
+        error_messages = {
+            'nombre' : {
+                'max_length' : ("Nombre demasiado largo. No debe exceder los 20 caracteres."),
+            },
+            'descripcion' : {
+                'max_length' : ("Descripcion demasiado larga. No debe exceder los 50 caracteres."),
+            },
+            'marca' : {
+                'max_length' : ("Nombre de marca demasiado largo. No debe exceder los 20 caracteres."),
+            }
         }
         widgets = {
             'nombre': forms.TextInput(attrs={'class':'form-control col-md-7 col-xs-12'}),
             'descripcion': forms.TextInput(attrs={'class':'form-control col-md-7 col-xs-12'}),
             'marca': forms.TextInput(attrs={'class':'form-control col-md-7 col-xs-12'}),
-            'stock_minimo': forms.NumberInput(attrs={'class':'form-control col-md-7 col-xs-12'}),
-            'stock': forms.NumberInput(attrs={'class':'form-control col-md-7 col-xs-12'}),
-            'precio': forms.NumberInput(attrs={'class':'form-control col-md-7 col-xs-12'}),
+            'stock_minimo': forms.NumberInput(attrs={
+                'class':'form-control col-md-7 col-xs-12',
+                'data-validate-minmax': '0,',
+                }),
+            'stock': forms.NumberInput(attrs={
+                'class':'form-control col-md-7 col-xs-12',
+                'data-validate-minmax': '0,',
+                }),
+            'precio': forms.NumberInput(attrs={
+                'class':'form-control col-md-7 col-xs-12',
+                'data-validate-minmax': '0,',
+                }),
+        }
+
+class ProductoUpdateForm(ProductoForm):
+
+    class Meta(ProductoForm.Meta):
+        widgets = {
+            'nombre': forms.TextInput(attrs={
+                'class':'form-control col-md-7 col-xs-12',
+                'readonly':'True',
+                }),
+            'descripcion': forms.TextInput(attrs={
+                'class':'form-control col-md-7 col-xs-12'
+                }),
+            'marca': forms.TextInput(attrs={
+                'class':'form-control col-md-7 col-xs-12'
+                }),
+            'stock_minimo': forms.NumberInput(attrs={
+                'class':'form-control col-md-7 col-xs-12'
+                }),
+            'stock': forms.NumberInput(attrs={
+                'class':'form-control col-md-7 col-xs-12',
+                'readonly': 'True',
+                }),
+            'precio': forms.NumberInput(attrs={
+                'class':'form-control col-md-7 col-xs-12'
+                }),
         }
