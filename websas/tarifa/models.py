2cce8f7cfc3fd847540e7e9069249f6dc6850c76
LucianoFromTrelew <lucianoserruya@hotmail.com>
2017-10-24 19:58:26 -0300

    cambiado el campo de precio de tarifa

--- websas/tarifa/models.py
+++ websas/tarifa/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from tarea.models import Tarea
 from servicio.models import TipoServicio
+from decimal import Decimal
 # Create your models here.
 class Tarifa(models.Model):
     tarea = models.ForeignKey(
@@ -9,7 +10,7 @@ class Tarifa(models.Model):
     tipo_servicio = models.ForeignKey(
         TipoServicio, related_name="tarifas"
     )
-    precio = models.FloatField()
+    precio = models.DecimalField(decimal_places=2, max_digits=10, default=Decimal('0'))
 
     class Meta:
         unique_together = (("tarea", "tipo_servicio"),)
