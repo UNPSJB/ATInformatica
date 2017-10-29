9dfddb0d5a0ba57b4d8d2ba984f615b433611ee6
Pablo <pabloatm980@gmail.com>
2017-10-29 13:15:06 -0300

    datatables de rubro

---
+++ websas/orden/static/js/rubro_ot.js
@@ -0,0 +1,37 @@
+
+$(document).ready(function(){
+    tabla_rubros()
+})
+
+// Inicializacion y referencia al DataTable, con la config ajax de cada columna
+function tabla_rubros(){
+    var tabla_html = $("#datatable-rubros")
+
+    // Inicializacion y referencia al DataTable, con la config ajax de cada columna
+    var dtabla = tabla_html.DataTable({
+        "responsive": true,
+        "columns": [
+            {"data": ""},
+            {"data": "nombre"},
+            {"data": "descripcion"},
+        ],
+        "dom": "Bfrtip",
+        "buttons": [{
+            extend: "copy",
+            text: "Copiar tabla",
+            className: "btn-sm"
+        }, {
+            extend: "csv",
+            text: "Exportar tabla a CSV",
+            className: "btn-sm"
+        }, {
+            extend: "print",
+            text: "Imprimir tabla",
+            className: "btn-sm"
+        }, ],
+    });
+
+    console.log('dtabla')
+    console.log(dtabla)
+    return dtabla
+}
