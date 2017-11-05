affb1f26a6239a5fddf254d35161f2cc07be1455
Kevin Soto <sotokevin92@users.noreply.github.com>
2017-11-05 20:50:57 -0300

    template Rubro - chequear permisos CRUD

--- websas/rubro/static/rubro/js/rubros.js
+++ websas/rubro/static/rubro/js/rubros.js
@@ -1,18 +1,39 @@
-var tabla = $("#datatable-rubros").DataTable({
+var tabla_html = $("#datatable-rubros");
+
+var botones_default = [{
+    extend: "copy",
+    text: "Copiar tabla",
+    className: "btn-sm"
+}, {
+    extend: "csv",
+    text: "Exportar tabla a CSV",
+    className: "btn-sm"
+}, {
+    extend: "print",
+    text: "Imprimir tabla",
+    className: "btn-sm"
+}];
+
+var botones_dt;
+
+console.log(tabla_html.attr('data-url_agregar'))
+
+if (tabla_html.attr('data-url_agregar')) {
+    var boton_agregar = [{
+        text: "<b>Nuevo rubro</b>",
+        action: function() {
+            location.href = tabla_html.attr('data-url_agregar');
+        },
+        className: "btn-sm btn-info"
+    }]
+
+    botones_dt = boton_agregar.concat(botones_default);
+} else {
+    botones_dt = botones_default;
+}
+
+tabla_html.DataTable({
     responsive: true,
-    keys: true,
     dom: "Bfrtip",
-    buttons: [{
-        extend: "copy",
-        text: "Copiar tabla",
-        className: "btn-sm"
-    }, {
-        extend: "csv",
-        text: "Exportar tabla a CSV",
-        className: "btn-sm"
-    }, {
-        extend: "print",
-        text: "Imprimir tabla",
-        className: "btn-sm"
-    }, ],
+    buttons: botones_dt,
 });
