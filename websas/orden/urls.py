d1e276a6c8bd4a99e78fbaac5ab0a6b8c065f89e
LucasKrmpotic <lucas_krmpotic@yahoo.com.ar>
2017-11-12 22:18:48 +0000

    login_required en views OT y Tarea

--- websas/orden/urls.py
+++ websas/orden/urls.py
@@ -3,19 +3,19 @@ from django.conf.urls import url
 from .views import OrdenCreate, OrdenCerrar, OrdenCancelar, OrdenesList, OrdenDelete, OrdenDetail, ClienteListado,EquipoCreate, EquipoList, EquipoDelete,EquipoCreatePopUp,EquipoListado
 
 urlpatterns = [
-    url(r'^crear$', OrdenCreate.as_view(), name="orden_crear"),
-    url(r'^cerrar$', OrdenCerrar.as_view(), name="orden_cerrar"),
-    url(r'^cancelar$', OrdenCancelar.as_view(), name="orden_cancelar"),
-    url(r'^listar$', OrdenesList.as_view(), name="orden_listar"),
-    url(r'^eliminar/(?P<pk>\d+)$', OrdenDelete.as_view(), name="orden_eliminar"),
-    url(r'^ver/(?P<pk>\d+)$', OrdenDetail.as_view(), name="orden_ver"),
-    url(r'^lista_clientes$', ClienteListado.as_view(), name="cliente_listar"),
-    url(r'^lista_equipos$', EquipoListado.as_view(), name="equipo_listar"),
-    url(r'^equipo/crear$',EquipoCreate.as_view(),name='equipo_crear'),
-    url(r'^equipo/listar$',EquipoList.as_view(),name='equipo_listar'),
-    url(r'^equipo/eliminar/(?P<pk>\d+)$',EquipoDelete.as_view(),name='equipo_eliminar'),
-    url(r'^equipo/ver/(?P<pk>\d+)$',EquipoList.as_view(),name='equipo_ver'),
-    url(r'^equipo/editar/(?P<pk>\d+)$',EquipoList.as_view(),name='equipo_editar'),
-    url(r'^equipo/crear_popup$',EquipoCreatePopUp.as_view(),name='equipo_crear_popup')
+    url(r'^crear$', login_required(OrdenCreate.as_view(), login_url='usuario:login'), name="orden_crear"),
+    url(r'^cerrar$', login_required(OrdenCerrar.as_view(), login_url='usuario:login'), name="orden_cerrar"),
+    url(r'^cancelar$', login_required(OrdenCancelar.as_view(), login_url='usuario:login'), name="orden_cancelar"),
+    url(r'^listar$', login_required(OrdenesList.as_view(), login_url='usuario:login'), name="orden_listar"),
+    url(r'^eliminar/(?P<pk>\d+)$', login_required(OrdenDelete.as_view(), login_url='usuario:login'), name="orden_eliminar"),
+    url(r'^ver/(?P<pk>\d+)$', login_required(OrdenDetail.as_view(), login_url='usuario:login'), name="orden_ver"),
+    url(r'^lista_clientes$', login_required(ClienteListado.as_view(), login_url='usuario:login'), name="cliente_listar"),
+    url(r'^lista_equipos$', login_required(EquipoListado.as_view(), login_url='usuario:login'), name="equipo_listar"),
+    url(r'^equipo/crear$', login_required(EquipoCreate.as_view(), login_url='usuario:login'),name='equipo_crear'),
+    url(r'^equipo/listar$', login_required(EquipoList.as_view(), login_url='usuario:login'),name='equipo_listar'),
+    url(r'^equipo/eliminar/(?P<pk>\d+)$', login_required(EquipoDelete.as_view(), login_url='usuario:login'),name='equipo_eliminar'),
+    url(r'^equipo/ver/(?P<pk>\d+)$', login_required(EquipoList.as_view(), login_url='usuario:login'), name='equipo_ver'),
+    url(r'^equipo/editar/(?P<pk>\d+)$', login_required(EquipoList.as_view(), login_url='usuario:login'), name='equipo_editar'),
+    url(r'^equipo/crear_popup$', login_required(EquipoCreatePopUp.as_view(), login_url='usuario:login'), name='equipo_crear_popup')
 ]
 
--- websas/tarea/urls.py
+++ websas/tarea/urls.py
@@ -2,10 +2,10 @@ from django.contrib.auth.decorators import login_required
 from django.conf.urls import url
 from .views import ReservaCreate, TareaFinalizar, ObservacionCreate, TareaAceptar, TareaCreate, TareaDetail, TipoTareaCreate, TipoTareaDelete, TipoTareaUpdate
 urlpatterns = [
-    url(r'^agregar$', TareaCreate.as_view(), name='tarea_crear'),
-    url(r'^aceptar$', TareaAceptar.as_view(), name='tarea_aceptar'),
-    url(r'^finalizar$', TareaFinalizar.as_view(), name='tarea_finalizar'),
-    url(r'^detalle/(?P<pk>\d+)$', TareaDetail.as_view(), name='tarea_ver'),
-    url(r'^reservar$', ReservaCreate.as_view(), name='reserva_crear'),
-    url(r'^observar$', ObservacionCreate.as_view(), name='observacion_crear'),
+    url(r'^agregar$', login_required(TareaCreate.as_view(), login_url='usuario:login'), name='tarea_crear'),
+    url(r'^aceptar$', login_required(TareaAceptar.as_view(), login_url='usuario:login'), name='tarea_aceptar'),
+    url(r'^finalizar$', login_required(TareaFinalizar.as_view(), login_url='usuario:login'), name='tarea_finalizar'),
+    url(r'^detalle/(?P<pk>\d+)$', login_required(TareaDetail.as_view(), login_url='usuario:login'), name='tarea_ver'),
+    url(r'^reservar$', login_required(ReservaCreate.as_view(), login_url='usuario:login'), name='reserva_crear'),
+    url(r'^observar$', login_required(ObservacionCreate.as_view(), login_url='usuario:login'), name='observacion_crear'),
 ]
