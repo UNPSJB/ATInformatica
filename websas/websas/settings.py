7ab83aaef2f81c1e1a65b255d6488af3fe05f4f8
Kevin Soto <sotokevin92@users.noreply.github.com>
2017-11-14 06:49:59 -0300

    Front - admin-fix

--- websas/websas/settings.py
+++ websas/websas/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'websas.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'admin/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
