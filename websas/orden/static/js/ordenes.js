db543cb31e01e7683a38bdd3e7dbc775ab3c3f9d
Kevin Soto <sotokevin92@users.noreply.github.com>
2017-11-17 16:38:55 -0300

    Front - tabla de OTs
    Ordenadas de la más reciente a la más antigua. Prioridades responsive para que la fecha y los botones de ver y eliminar se muestren SIEMPRE. Campo Descripción truncado a 50 caracteres con modal que muestra el texto completo.

--- websas/orden/static/js/ordenes.js
+++ websas/orden/static/js/ordenes.js
@@ -1,6 +1,35 @@
-$("#datatable-ordenes").DataTable({
+var tabla_html = $('#datatable-ordenes');
+var modal_observacion = $('#modal-observacion');
+var modal_observacion_texto = $('#texto-observacion-completa');
+var observaciones_truncadas = {};
+
+// Truncate a string
+function strtrunc(str, max, add){
+    add = add || '... (ver todo)';
+    return (typeof str === 'string' && str.length > max ? str.substring(0, max - add.length) + add : str);
+ };
+
+tabla_html.DataTable({
     responsive: true,
     dom: "Bfrtip",
+    order: [[ 0, 'desc' ]],
+    columnDefs: [
+        {
+            'targets': 2,
+            'render': function(data, type, full, meta){
+                if (data.length > 50){
+                    var obs_corta = strtrunc(data, 50);
+                    var fila = meta.row;
+                    observaciones_truncadas[fila] = data;
+                    var prefijo = '<a href=# onclick="mostrar_observacion(\'' + fila + '\')">';
+                    var sufijo = '</a>';
+                    data = prefijo + obs_corta + sufijo;
+                }
+                
+                return data;
+            }
+        }
+    ],
     buttons: [{
         text: "<b>Nueva orden</b>",
         action: function() {
@@ -24,3 +53,8 @@ $("#datatable-ordenes").DataTable({
         className: "btn-sm"
     }, ],
 });
+
+function mostrar_observacion(clave) {
+    modal_observacion_texto.html(observaciones_truncadas[clave]);
+    modal_observacion.modal();
+}
