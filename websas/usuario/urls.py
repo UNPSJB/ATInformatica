99ca61694b6f243fe83ca72be0ee2765c27d1294
LucianoFromTrelew <lucianoserruya@hotmail.com>
2017-11-23 18:59:24 -0300

    permite eliminar permisos de un grupo y usuarios de un grupo

--- websas/usuario/urls.py
+++ websas/usuario/urls.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.conf.urls import url
-from .views import UserAddGroup, GroupAddPermission,  RegistrarUsuario, GroupView, LoginView, LogoutView, CambiarContraseñaView, CambiarContraseñaOKView
+from .views import UserAddGroup, UserRemoveGroup, GroupAddPermission, GroupRemovePermission, RegistrarUsuario, GroupView, LoginView, LogoutView, CambiarContraseñaView, CambiarContraseñaOKView
 from django.contrib.auth.views import (
     PasswordChangeView,
     PasswordChangeDoneView,
@@ -12,7 +12,9 @@ urlpatterns = [
     url(r'crear$', login_required(RegistrarUsuario.as_view(), login_url='usuario:login'), name="usuario_crear"),
     url(r'grupos$', login_required(GroupView.as_view(), login_url='usuario:login'), name="grupos"),
     url(r'add_grupo$', login_required(UserAddGroup.as_view(), login_url='usuario:login'), name="usuario_add_grupo"),
+    url(r'delete_grupo$', login_required(UserRemoveGroup.as_view(), login_url='usuario:login'), name="usuario_delete_grupo"),
     url(r'add_permiso$', login_required(GroupAddPermission.as_view(), login_url='usuario:login'), name="grupo_add_permiso"),
+    url(r'delete_permiso$', login_required(GroupRemovePermission.as_view(), login_url='usuario:login'), name="grupo_delete_permiso"),
     url(r'^password_change/$', login_required(CambiarContraseñaView.as_view(), login_url='usuario:login'), name='password_change'),
     url(r'^password_change/done/$', login_required(CambiarContraseñaOKView.as_view(), login_url='usuario:login'), name='password_change_done'),
 ]
