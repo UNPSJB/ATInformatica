45fe1649db12b31eecf42c0543f3cc1190ad90d0
LucianoFromTrelew <lucianoserruya@hotmail.com>
2017-12-01 13:54:26 -0300

    importes de OTs por cliente en reportes.views

--- websas/reportes/views.py
+++ websas/reportes/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 # Create your views here.
+from django.db.models import Sum, F, Value
+from django.db.models.functions import Concat, Upper
 
-
+from orden.models import Orden
 
 
 class Reportes(TemplateView):
     template_name = "reportes/reportes.html"
+
+
+    def get(self, request, *args, **kwargs):
+        total_ordenes_por_cliente = Orden.objects.values(
+            propietario=Concat(Upper(F("cliente__persona__apellido")),
+                               Value(", "),
+                               F("cliente__persona__nombre"))).annotate(
+                               total=Sum("precio_final"))
+
+        return super().get(request, *args, **kwargs)
