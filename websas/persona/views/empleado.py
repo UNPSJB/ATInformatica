82ead80d3e0ec3144c49c2b2393c002c57b1fcd7
LucasKrmpotic <lucas_krmpotic@yahoo.com.ar>
2017-12-02 16:17:26 +0000

    arreglo en persona

--- websas/persona/views/empleado.py
+++ websas/persona/views/empleado.py
@@ -22,7 +22,7 @@ class EmpleadoCreate(CreateView):
         self.object = self.get_object
         form = self.form_class(request.POST)
         if form.is_valid():
-            persona = form.save(commit=False)
+            persona = form.save()
             persona.agregar_rol(self._rol)
             persona.save()
             return HttpResponseRedirect(self.get_success_url())
