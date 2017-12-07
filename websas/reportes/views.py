e8a92f529b71e0865b18f6b5154d67701b330f21
Pablo <pabloatm980@gmail.com>
2017-12-07 09:35:26 -0300

    Devuelve total facturado por cliente, cantidad de OT por cliente y por Tecnico

--- websas/reportes/views.py
+++ websas/reportes/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.generic import TemplateView
 # Create your views here.
-from django.db.models import Sum, F, Value
+from django.db.models import Sum, Count, F, Value
 from django.db.models.functions import Concat, Upper
 from datetime import datetime
 
@@ -29,14 +29,39 @@ class ReporteTotalOrdenesClientesRangoTiempo(TemplateView):
             fecha_ini = form.cleaned_data.get("fecha_ini")
             fecha_fin = form.cleaned_data.get("fecha_fin")
             
-            ordenes = Orden.objects.filter(
+            ordenes_total = Orden.objects.filter(
                 fecha_fin__range=[fecha_ini, fecha_fin]).values(
                     propietario=Concat(Upper(F("cliente__persona__apellido")),
                     Value(", "), 
                     F("cliente__persona__nombre"))).annotate(
                     total=Sum("precio_final"))
             
+            ordenes_cantidad_cliente = Orden.objects.filter(
+                fecha_fin__range=[fecha_ini, fecha_fin]).values(
+                    propietario=Concat(Upper(F("cliente__persona__apellido")),
+                    Value(", "), 
+                    F("cliente__persona__nombre"))).annotate(
+                    total=Count("precio_final"))
+
+            ordenes_cantidad_tecnico = Orden.objects.filter(
+                fecha_fin__range=[fecha_ini, fecha_fin]).values(
+                    creador=Concat(Upper(F("tecnico__persona__apellido")),
+                    Value(", "), 
+                    F("tecnico__persona__nombre"))).annotate(
+                    total=Count("precio_final"))
+
+
+            print(ordenes_total)
+            print(ordenes_cantidad_cliente)
+            print(ordenes_cantidad_tecnico)
                 
-            return JsonResponse({"ordenes": list(ordenes)})
+            return JsonResponse(
+                {
+                    "ordenes_total": list(ordenes_total),
+                    "ordenes_cantidad_cliente": list(ordenes_cantidad_cliente),
+                    "ordenes_cantidad_tecnico": list(ordenes_cantidad_tecnico)
+                }
+            )
+
 
         return JsonResponse({})
